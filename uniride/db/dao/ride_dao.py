1ad5610878df34c87764fe9f1cce5ba4c5e758c6
Tomer Rozen <tomerrzn@gmail.com>
2023-06-08 21:10:59 +0300

    Add dao for get

--- uniride/db/dao/ride_dao.py
+++ uniride/db/dao/ride_dao.py
@@ -2,6 +2,7 @@
 from fastapi import Depends
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from uniride.db.dependencies import get_db_session
 from uniride.db.models.ride_model import RideModel
@@ -20,3 +21,18 @@ class RideDAO:
         :param name: name of a ride.
         """
         self.session.add(RideModel(**kwargs))
+
+    async def get_ride_model_by_id(
+        self,
+        id: int,
+    ) -> RideModel:
+        """
+        Get specific dummy model.
+
+        :param id: id of ride instance.
+        :return: ride models.
+        """
+        query = select(RideModel)
+        query = query.where(RideModel.id == id)
+        rows = await self.session.execute(query)
+        return rows.scalars().first()
