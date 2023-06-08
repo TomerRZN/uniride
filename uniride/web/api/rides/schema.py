e5ed5ba382915b97041b871f6c71facd6e2ddc89
amit weinstock <amit.weinstock@ridewithvia.com>
2023-06-08 21:49:04 +0300

    with get_ride_by_id and model

--- uniride/web/api/rides/schema.py
+++ uniride/web/api/rides/schema.py
@@ -1,28 +1,26 @@
 from pydantic import BaseModel
 
-#
-# class RideModelDTO(BaseModel):
-#     """
-#     DTO for dummy models.
-#
-#     It returned when accessing dummy models from the API.
-#     """
-#
-#     driver_id: int
-#     name: str
-#     image: str
-#     event_time: datetime
-#     distance: float  # dynamic
-#     ride_id: int
-#     location: Location
-#     seats: int
-#     to_uni: bool
-#     passengers: List[Profile]
-#     price: float
-#     description: str
-#
-#     class Config:
-#         orm_mode = True
+
+class RideModelDTO(BaseModel):
+    """
+    DTO for dummy models.
+
+    It returned when accessing dummy models from the API.
+    """
+    id: int
+    profile_id: int
+    name: str
+    image: str
+    event_time: str
+    location: str
+    seats: int
+    to_uni: bool
+    passengers: list[int]
+    price: float
+    description: str
+
+    class Config:
+        orm_mode = True
 
 
 class RideModelInputDTO(BaseModel):
--- uniride/web/api/rides/views.py
+++ uniride/web/api/rides/views.py
@@ -2,7 +2,10 @@ from fastapi import APIRouter
 from fastapi.param_functions import Depends
 
 from uniride.db.dao.ride_dao import RideDAO
+from uniride.db.models.ride_model import RideModel
 from uniride.web.api.rides.schema import RideModelInputDTO
+from uniride.web.api.rides.schema import RideModelDTO
+from fastapi.exceptions import HTTPException
 
 router = APIRouter()
 
@@ -21,3 +24,32 @@ async def create_ride(
     """
     # Replace the logic below with your actual implementation
     await ride_dao.create_ride_model(**new_ride.dict())
+
+
+@router.get("/ride/{id}", response_model=RideModelDTO)
+async def get_ride_by_id(
+    id: int,
+    ride_dao: RideDAO = Depends()
+) -> RideModel:
+    """
+    Create a new ride.
+
+    :param id: id of the ride
+    :param ride_dao: DAO for ride models.
+    :return: return ride object
+    """
+    ride = await ride_dao.get_ride_model_by_id(id)
+    if not ride:
+        raise HTTPException(404)
+
+    # TODO: add passangers and also decrease seats
+    # TODO: add get_profile to get image and name of profile_id
+    ride.name = ''
+    ride.image = ''
+    ride.passengers = []
+
+    if ride.location == 'Herzliya':
+        ride.distance = 5.6
+    elif ride.location == 'Tel Aviv':
+        ride.distance = 0.8
+    return ride
